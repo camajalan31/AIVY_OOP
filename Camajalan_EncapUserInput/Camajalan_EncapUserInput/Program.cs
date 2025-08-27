9ec09f864d44c3bf15aa867bed0c3ef8fd1c837d
ACT-STUDENT <ACT-STUDENT@LAB3-PC15>
2025-08-27 14:41:13 +0800

    Exercise 5

---
+++ Camajalan_EncapUserInput/Camajalan_EncapUserInput/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_EncapUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Main:
+            Console.Clear();
+            Console.WriteLine("\tThis program will ask you about Shoe details.\n\n");
+            Console.Write("Brand Name: \t");
+            string bName = Console.ReadLine();
+
+            Console.Write("Price: \t\t");
+            string bPrice = Console.ReadLine();
+
+
+
+            //Instantiate the Shoe Class
+            Shoe myShoe = new Shoe();
+
+            myShoe.Brand = bName;
+            myShoe.Price = bPrice;
+
+            //Call the Method to display  the Shoe details Information
+            myShoe.DisplayShoeDetails();
+
+            //Input another Shoe details
+            Console.WriteLine("\n Do you want to input another Shoe details (yes/no)?  ");
+            string ans = Console.ReadLine();
+
+            if (ans == "yes")
+            {
+                goto Main;
+            }
+            else if (ans == "no")
+            {
+                return;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ Camajalan_EncapUserInput/Camajalan_EncapUserInput/Shoe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_EncapUserInput
+{
+    internal class Shoe
+    {
+        //Declaration of fields
+        public string Brand { get; set; }
+        public string Price { get; set; }
+
+        public void DisplayShoeDetails()
+        {
+            Console.Clear();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("----Shoe Details----");
+            Console.WriteLine("Brand Name: " + Brand);
+            Console.WriteLine("Price: " + Price);
+
+
+
+        }
+    }
+}
