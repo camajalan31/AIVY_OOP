618ffa2b49a4d898d977e66ba395f8a70946cf25
ACT-STUDENT <ACT-STUDENT@LAB3-PC15>
2025-08-29 14:38:45 +0800

    exercise2

---
+++ Camajalan_Class&Objects/Camajalan_Class&Objects/Students.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_Class_Objects
+{
+    internal class Students
+    {
+        //Class Atttributes
+        public string name;
+        public string section;
+        public string course;
+
+        //Method to display Information
+        public void DisplayInfo()
+        {
+            Console.Write("Hi I'm " + name + " from section " + section + " currently taking " + course + " 2nd year level.");
+        }
+
+    }
+}
