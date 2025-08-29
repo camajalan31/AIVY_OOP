0bbe05929fb3d1ffa6fe65fe0bae9cae705de8cc
ACT-STUDENT <ACT-STUDENT@LAB3-PC15>
2025-08-29 14:27:22 +0800

    exercise1_oop

---
+++ Camajalan_Ex1/Camajalan_Ex1/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_Ex1
+{
+    internal class Person
+    {
+        //Class Members
+        public string name;
+        public int age;
+        public string gender;
+
+        //Method to Display the fields
+        public void Greet()
+        {
+            Console.WriteLine("Good Day everyone, I'm " + name + " " + age + "years old. I'm a " + gender + ".");
+        }
+
+    }
+}
