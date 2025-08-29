742ba98f6e51c805c8228b56ad78194564cd4e3f
Aivy Camajalan <camajalanaivy63@gmail.com>
2025-08-29 14:32:01 +0800

    exercise2

---
+++ Camajalan_MyClassAndObjects/Camajalan_MyClassAndObjects/Students.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_MyClassAndObjects
+{
+    internal class Students
+    {
+        //Class Atttributes
+        public string name;
+        public string section;
+        public string course;
+
+        //Method to display Information
+        public void DisplayInfo()
+        {
+            Console.Write("Hi I'm " + name + " from section " + section + " currently taking " + course + " 2nd year level.");
+        }
+    }
+}
