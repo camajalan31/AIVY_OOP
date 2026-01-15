603c71c1fea94ae38e6027a21a4b5c6aa9babf1f
Aivy Camajalan <camajalanaivy63@gmail.com>
2026-01-15 17:00:11 +0800

    Add files via upload

---
+++ Camajalan_SimpleInheritance/Camajalan_SimpleInheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_SimpleInheritance
+{
+    internal class Program
+    {
+        //Aivy Camajalan, IT306
+        //Title:Simple Inheritance
+        static void Main(string[] args)
+        {         
+            //Create an instance of the regular student class
+            RegularStudent regularStudent = new RegularStudent 
+            {
+                Name = "Aivy Camajalan",
+                Program = "BSIT",
+                Section = "IT306"
+            };
+            regularStudent.BasicInfo();// call inherited method to display student information
+            regularStudent.SectionEnrolled(); //call method to display regular students details
+
+            //Create an instance of the irregular student class
+            IrregularStudent irregularStudent = new IrregularStudent
+            {
+                Name = "Mary Joyce Camero",
+                Program = "BSIT",
+                UnitsEnrolled = "18 units"
+            };
+            irregularStudent.BasicInfo();
+            irregularStudent.EnrolledSemUnits();//call method to display irregular student
+
+            Console.ReadKey();  
+
+        }
+    }
+}
---
+++ Camajalan_SimpleInheritance/Camajalan_SimpleInheritance/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_SimpleInheritance
+{
+    class Student // Base Class      
+    {
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        //Method to display student information
+        public void BasicInfo()
+        {
+            Console.WriteLine("\n -----Student Information-----\n");
+            Console.WriteLine($"Name: {Name} \nProgram:  {Program}");
+        }
+    }
+    class RegularStudent : Student //Derived Class
+    { 
+            //Additional property for RegularStudent class
+            public string Section { get; set; }
+
+            //Method to display RegularStudent information
+            public void SectionEnrolled()
+            { 
+             Console.WriteLine($"Section: {Section}");
+            }
+    }        
+    class IrregularStudent : Student //Derived Class
+    { 
+           //Additional property for IrregularStudent class
+           public string UnitsEnrolled { get; set; }
+
+            //Method to display IrregularStudent Information
+            public void EnrolledSemUnits() 
+            { 
+             Console.WriteLine($"UnitsEnrolled: {UnitsEnrolled}");
+            }
+     }
+}
