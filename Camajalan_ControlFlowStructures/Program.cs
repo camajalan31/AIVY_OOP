c0e0211f7cac46cf4e3b2ef2b47959e799139895
Aivy Camajalan <camajalanaivy63@gmail.com>
2026-01-15 16:59:29 +0800

    Add files via upload

---
+++ Camajalan_ControlFlowStructures/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_ControlFlowStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        //Aivy Camajalan IT306
+        //Problem: Check if a number entered by the user is between a certain range of numbers (inclusive).
+        {
+
+            // users input a number
+            int LowRange = 50;
+            Console.WriteLine("The range is between 1-50 only. Enter a number and it will identify if its range of number or not.");
+           
+
+            // enter a number to identify if it is between the range or not+
+            Console.Write("Enter a num: ");
+            LowRange = Convert.ToInt32(Console.ReadLine());
+
+            //setting the range of a number and displaying the output whether it is  between the range or not.
+            if ( LowRange <= 50 ) 
+            {
+                Console.WriteLine("The number is between the range");
+            }
+            else
+            {
+                Console.WriteLine("The number is not between the range");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Camajalan_ControlFlowStructures/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Camajalan_ControlFlowStructures")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Camajalan_ControlFlowStructures")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b83fd5b0-3d68-4091-be04-63809d75ae85")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
