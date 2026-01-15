5e4b5c8dc4c3bfa7e697a4f76acc14998dc3b35e
Aivy Camajalan <camajalanaivy63@gmail.com>
2026-01-15 17:04:39 +0800

    Add files via upload

---
+++ Camajalan_InheritanceWithConstructor/Camajalan_InheritanceWithConstructor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_InheritanceWithConstructor
+{
+    internal class Program
+    {
+        //Aivy Camajalan, IT306
+        //Title: Simple Inheritance with Constructor
+        static void Main(string[] args)
+        {
+            //Create an instance of the Student Class
+            Student student = new Student("Aivy Camajalan", "BSIT");
+
+            student.DisplayStudType();
+
+            //Create an instance if the regular student class
+            RegularStudent regularstudent = new RegularStudent("Aivy Camajalan", "BSIT", "IT306");
+            regularstudent.SectionEnrolled();//Call method to display regular student info
+
+            //Create an instance of the irregular student class
+            IrregularStudent irregularstudent = new IrregularStudent("Mary Joyce Camero", "BSIT", "IT306", "18 units");
+            irregularstudent.EnrolledSemUnits();//Call method to display irregular student info
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Camajalan_InheritanceWithConstructor/Camajalan_InheritanceWithConstructor/Student.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_InheritanceWithConstructor
+{
+     class Student
+    {
+        //Declaration of the fields
+        public string name;
+        public string program;
+
+        //Constructor
+        public Student(string studentName, string studentProgram)
+        {
+            name = studentName;
+            program = studentProgram;
+        }
+        public void DisplayStudType()
+        {
+            Console.WriteLine("\n  ---Student Information---");
+        }
+        public void BasicInfo()
+        {
+            Console.WriteLine($"     Name: {name} \n     Program: {program}");
+        }
+    }
+
+    class RegularStudent : Student
+    {
+        //fields
+        public string Section;
+        //constructor
+        public RegularStudent(string studentName, string studentProgram, string regularstudentSection) : base(studentName, studentProgram)
+        {
+
+            Section = regularstudentSection;
+        }
+        public void DisplayRegStudType()
+        {
+            Console.WriteLine("\n\n-----Regular Student Information-----\n");
+        }
+
+        public void SectionEnrolled()
+        {
+            DisplayRegStudType();//Call method to display the stud type
+            BasicInfo();//Call inherited method to display student info
+            Console.WriteLine($"     Section: {Section}");
+        }
+
+    }
+
+    class IrregularStudent : Student
+    {
+        //fields
+        public string UnitsEnrolled;
+        //Constructor
+        public IrregularStudent(string studentName, string studentProgram, string regularstudentSection, string irregularstudentUnitsEnrolled) : base(studentName, studentProgram)
+        {
+            UnitsEnrolled = irregularstudentUnitsEnrolled;
+        }
+        public void DisplayIrregStudType()
+        {
+            Console.WriteLine("\n\n-----Irregular Student Information-----\n");
+        }
+
+        public void EnrolledSemUnits()
+        {
+            DisplayIrregStudType();//Call method to display the stud type
+            BasicInfo();//Call inherited method to display student info
+            Console.WriteLine($"     UnitsEnrolled: {UnitsEnrolled}");
+        }
+    }
+}
