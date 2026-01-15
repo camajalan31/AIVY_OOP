bcc78a385cdd7c6baa5790a3ed55cc5205b59bf8
Aivy Camajalan <camajalanaivy63@gmail.com>
2026-01-15 17:02:17 +0800

    Add files via upload

---
+++ Camajalan/Camajalan/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan
+{
+    internal class Person
+    {
+        public string Name, Gender;
+        public int Age;
+        
+        public Person(string Name1, int Age1, string Gender1) 
+        {
+            Name = Name1;
+            Age= Age1;
+            Gender =Gender1;    
+
+        }
+    }
+}
---
+++ Camajalan/Camajalan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person ("Aybi", 18, "Girl"); 
+
+            //call method
+            Console.WriteLine ("Personal Information:");
+            Console.WriteLine ("Name:"+person1.Name +"\nAge: " + person1.Age+ "\nGender: "  + person1.Gender );
+
+            Console.ReadLine ();
+        }
+    }
+}
