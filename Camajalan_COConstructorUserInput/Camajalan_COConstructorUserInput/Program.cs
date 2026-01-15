668e96a91c7068c4c396f35def8d30df396cc10e
Aivy Camajalan <camajalanaivy63@gmail.com>
2026-01-15 16:58:53 +0800

    Add files via upload

---
+++ Camajalan_COConstructorUserInput/Camajalan_COConstructorUserInput/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_COConstructorUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //Aivy Camajalan_IT306
+        //Title:User Input with Constructor
+        Main:
+            Console.Clear();
+            Console.WriteLine("This program will ask you about student details.\n\n");
+            Console.Write("Name of student: \t");
+            string vName = Console.ReadLine();
+
+            Console.Write("Section of the student: \t");
+            string vSection = Console.ReadLine();
+
+            Console.Write("Course of the student: \t");
+            string vCourse = Console.ReadLine();    
+
+            //Initiate a class object
+            Students students1 = new Students (vName, vSection, vCourse);
+
+            //Call the Method to display  the Student details Information
+            students1.DisplayStudentsDetails();
+
+            //Input another Student details
+            Console.WriteLine("\n Do you want to input another student details (yes/no)?  ");
+            string ans =Console.ReadLine();
+
+            if (ans == "yes") 
+            { 
+                goto Main;
+            }
+            else if (ans == "no")
+            {
+                return;
+            }
+                
+        }
+    }
+}
+ 
---
+++ Camajalan_COConstructorUserInput/Camajalan_COConstructorUserInput/Students.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camajalan_COConstructorUserInput
+{
+    internal class Students
+    {
+        //I'm changing my class and is not the same on exercise 2
+        // Declaration of the fields
+        public string name;
+        public string section;
+        public string course;
+
+        public Students(string vName, string vSection, string vCourse)
+        {
+            name = vName;
+            section = vSection;
+            course = vCourse;
+        }
+        public void DisplayStudentsDetails()
+        {
+            Console.Clear();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("----Student Details----");
+            Console.WriteLine("Student Name: " + name);
+            Console.WriteLine("Student Section: " + section);
+            Console.WriteLine("Student Course: " + course);
+
+
+        }
+    }
+}
